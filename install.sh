1718465b9bafb3077f396878d62671d4ad7325c9
Kydlaw <julien.coche@pm.me>
2020-08-16 16:18:34 -0400

    Minor updates

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # Initialisation
 sudo apt upgrade
 sudo apt update
-sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev git xclip
+sudo apt install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev git xclip
 sudo apt install python3
 # Zsh
 sudo apt install zsh fonts-firacode
